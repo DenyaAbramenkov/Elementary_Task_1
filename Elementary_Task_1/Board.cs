d08ff6fc6fdd2959a78b4817f2054c7824efc718
Denis Abramenkov <40663161+DenyaAbramenkov@users.noreply.github.com>
2018-10-08 19:42:09 +0300

    Initial commit

---
+++ Elementary_Task_1/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elementary_Task_1
+{
+    class Board
+    {
+        public int Board_Height { get; set; }
+        public int Board_Width { get; set; }
+        public Cell[,] Cells;
+        public  Board Build_Board()
+        {
+            Board board = new Board();
+            for (int i =0; i < Board_Height; i++)
+            {
+                for (int j = 0; j < Board_Height; j++)
+                {
+                    Cells[i, j] = new Cell();
+                }
+            }
+            return board;
+        }
+    }
+}
---
+++ Elementary_Task_1/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elementary_Task_1
+{
+    class Cell
+    {
+        public int Height_Location { get; set; }
+        public int Width_Location { get; set; }
+        public int Color { get; set; }
+    }
+}
